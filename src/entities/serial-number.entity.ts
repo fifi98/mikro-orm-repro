5573886012fc9785d4d3318ec7356c8245596cc9
fifi98 <bel.filip@icloud.com>
2022-12-02 12:21:25 +0100

    remove item entity

--- src/entities/serial-number.entity.ts
+++ src/entities/serial-number.entity.ts
@@ -7,16 +7,12 @@ import {
 } from '@mikro-orm/core';
 
 import { BaseEntity } from './base.entity';
-import { Item } from './item.entity';
 import { Log } from './log.entity';
 
 @Entity()
 export class SerialNumber extends BaseEntity {
   @Property()
   serialNumber: number;
-
-  @ManyToOne(() => Item)
-  item: Item;
 
   @OneToMany(() => Log, (log) => log.serialNumber)
   logs = new Collection<Log>(this);
